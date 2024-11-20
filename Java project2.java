5b625108c41dd8f468f4328b3f61ba33bc05d3d3
Raymond87866986 <pakv01987@gmail.com>
2024-11-20 07:45:28 +0330

    Version 2

---
+++ Java project2.java
@@ -0,0 +1,24 @@
+java
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[10];
+
+        
+        System.out.println("لطفاً ۱۰ اسم وارد کنید:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("اسم " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+
+  
+        System.out.println("\nتعداد کاراکترهای هر اسم:");
+        for (String name : names) {
+            System.out.println(name + ": " + name.length() + " کاراکتر");
+        }
+
+        scanner.close();
+    }
+}
